699105fdb07e3aaf2ad604b2a18eff4f11aec4ce
Fakhrialfi9 <Fakhrialfi9@gmail.com>
2024-07-23 18:54:26 +0700

    Prepare for deployment

--- src/database/skillData.js
+++ src/database/skillData.js
@@ -1,11 +1,11 @@
 const skillData = [
-  { id: 1, src: "../../public/logo/LogoLaravel.svg", alt: "Laravel Logo" },
-  { id: 2, src: "../../public/logo/LogoExpress.svg", alt: "Express Logo" },
-  { id: 3, src: "../../public/logo/LogoNode.svg", alt: "Node Logo" },
-  { id: 4, src: "../../public/logo/LogoReact.svg", alt: "React Logo" },
-  { id: 5, src: "../../public/logo/LogoViteJs.svg", alt: "ViteJs Logo" },
-  { id: 6, src: "../../public/logo/LogoAdobeIllustrator.svg", alt: "Adobe Illustrator Logo" },
-  { id: 7, src: "../../public/logo/LogoAdobePhotoShop.svg", alt: "Adobe Photoshop Logo" },
+  { id: 1, src: "fakhrialfi9/public/logo/LogoLaravel.svg", alt: "Laravel Logo" },
+  { id: 2, src: "fakhrialfi9/public/logo/LogoExpress.svg", alt: "Express Logo" },
+  { id: 3, src: "fakhrialfi9/public/logo/LogoNode.svg", alt: "Node Logo" },
+  { id: 4, src: "fakhrialfi9/public/logo/LogoReact.svg", alt: "React Logo" },
+  { id: 5, src: "fakhrialfi9/public/logo/LogoViteJs.svg", alt: "ViteJs Logo" },
+  { id: 6, src: "fakhrialfi9/public/logo/LogoAdobeIllustrator.svg", alt: "Adobe Illustrator Logo" },
+  { id: 7, src: "fakhrialfi9/public/logo/LogoAdobePhotoShop.svg", alt: "Adobe Photoshop Logo" },
 ];
 
 export default skillData;
